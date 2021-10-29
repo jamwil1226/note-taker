8c92e548d9d8cd6476792b0cbd3defb1eb75daaa
Jamie Williams <jamwil1226@yahoo.com>
2021-10-29 17:25:30 -0400

    edits to server.js

--- server.js
+++ server.js
@@ -11,14 +11,12 @@ app.use(express.static("public"));
 
 // Get/notes - needs to return notes.html
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "notes.html"));
-    console.log("Your Notes!");
+    res.sendFile(path.join(__dirname, "notes.html"));
 })
 
 // Get * - needs to return index.html (?)
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-    console.log("Your index!");
+    res.sendFile(path.join(__dirname, "index.html"));
 })
 
 app.get("/api/notes", (req, res) => {
@@ -40,9 +38,7 @@ app.post("/api/notes", function (req, res) {
             console.log("File read failed:", err)
             return
         }
-        console.log('File data:', jsonString);
-        // json.parse
-        var notes = JSON.parse(jsonString);
+        let notes = JSON.parse(jsonString);
 
         // Note object 
         const newNote = {
@@ -55,17 +51,12 @@ app.post("/api/notes", function (req, res) {
         
         // let noteText = [];
         notes.push(newNote);
-        // Will not push to newNote
         let NotesJSON = JSON.stringify(notes);
-        // push to array 
-        // then stringify 
-
+        
         fs.writeFile(path.join(__dirname, "db", "db.json"), NotesJSON, (err) => {
             if (err) {
                 return console.log(err);
             }
-            // this is console logging
-            console.log("Success!", NotesJSON);
             return NotesJSON;
         });
 
@@ -74,34 +65,29 @@ app.post("/api/notes", function (req, res) {
 });
 
 
-app.delete('/api/notes/:id', function (request, response) {
+app.delete('/api/notes/:id', function (req, res) {
 
     fs.readFile(path.join(__dirname, "db", "db.json"), 'utf8', (err, jsonString) => {
         if (err) {
             console.log("File read failed:", err)
             return
         }
-        console.log('File data:', jsonString);
-        // json.parse
-        var notes = JSON.parse(jsonString);
+        let notes = JSON.parse(jsonString);
 
         // Note object 
         const newNote = {
             title: req.body.title,
             text: req.body.text,
-            // Github code 
             id: Math.random().toString(36).substr(2, 9)
         };
 
-        notes.splice(request.params.id, 1);
+        notes.splice(req.params.id, 1);
         let NotesJSON = JSON.stringify(notes);
 
         fs.writeFile(path.join(__dirname, "db", "db.json"), NotesJSON, (err) => {
             if (err) {
                 return console.log(err);
             }
-            // this is console logging
-            console.log("Success!", NotesJSON);
             return NotesJSON;
         });
 
