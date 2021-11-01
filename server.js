e0cc82018f38be0b711749975e34ea806b87c24d
Jamie Williams <jamwil1226@yahoo.com>
2021-10-31 20:15:21 -0400

    add heroku

--- server.js
+++ server.js
@@ -1,102 +1,28 @@
-const express = require("express");
-const app = express();
-const fs = require("fs");
-const path = require("path");
-
+// Dependencies
+const express = require('express');
+// const path = require('path');
+// const fs = require('fs');
 const PORT = process.env.PORT || 3001;
+
+//Testing, not really needed here
+const dbJson = require('./db/db.json')
+
+// Sets up the Express App
+const app = express();
+
+//Accesses public file mainly for proper CSS loading
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('./'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
-// Get/notes - needs to return notes.html
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "notes.html"));
-})
-
-// Get * - needs to return index.html (?)
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-})
-
-app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "db", "db.json"), 'utf8', (err, jsonString) => {
-        if (err) {
-            console.log("File read failed:", err)
-            return
-        }
-        console.log('File data:', jsonString)
-        res.json(JSON.parse(jsonString));
-    })
-})
+//Require the Routes.js files in order to communicate when to generate api routes and html files
+require("./apiRoutes.js")(app);
+require("./htmlRoutes")(app);
 
 
-app.post("/api/notes", function (req, res) {
-
-    fs.readFile(path.join(__dirname, "db", "db.json"), 'utf8', (err, jsonString) => {
-        if (err) {
-            console.log("File read failed:", err)
-            return
-        }
-        let notes = JSON.parse(jsonString);
-
-        // Note object 
-        const newNote = {
-            title: req.body.title,
-            text: req.body.text,
-            // id
-            id: Math.random().toString(36).substr(2, 9)
-        };
-
-        
-        // let noteText = [];
-        notes.push(newNote);
-        let NotesJSON = JSON.stringify(notes);
-        
-        fs.writeFile(path.join(__dirname, "db", "db.json"), NotesJSON, (err) => {
-            if (err) {
-                return console.log(err);
-            }
-            return NotesJSON;
-        });
-
-    })
-
-});
-
-
-app.delete('/api/notes/:id', function (req, res) {
-
-    fs.readFile(path.join(__dirname, "db", "db.json"), 'utf8', (err, jsonString) => {
-        if (err) {
-            console.log("File read failed:", err)
-            return
-        }
-        let notes = JSON.parse(jsonString);
-
-        // Note object 
-        const newNote = {
-            title: req.body.title,
-            text: req.body.text,
-            id: Math.random().toString(36).substr(2, 9)
-        };
-
-        notes.splice(req.params.id, 1);
-        let NotesJSON = JSON.stringify(notes);
-
-        fs.writeFile(path.join(__dirname, "db", "db.json"), NotesJSON, (err) => {
-            if (err) {
-                return console.log(err);
-            }
-            return NotesJSON;
-        });
-
-    })
-
-});
-
-
-// Server listening confirmation
-app.listen(PORT, () => {
-    console.log(`Server is listening on PORT ${PORT}`);
+// Starts the server to begin listening
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
