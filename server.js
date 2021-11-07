ab308de56d2af0e977dd27688ecd6df98fcaa3ec
Jamie Williams <jamwil1226@yahoo.com>
2021-11-07 17:33:35 -0500

    add Heroku

--- server.js
+++ server.js
@@ -1,28 +1,80 @@
-// Dependencies
-const express = require('express');
-// const path = require('path');
-// const fs = require('fs');
 const PORT = process.env.PORT || 3001;
-
-//Testing, not really needed here
-const dbJson = require('./db/db.json')
-
-// Sets up the Express App
+const express = require('express');
 const app = express();
 
-//Accesses public file mainly for proper CSS loading
-app.use(express.static(__dirname + '/public'));
-app.use(express.static('./'));
+const path = require("path");
+const fs = require("fs");
+const notes = require("./db/db.json")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-//Require the Routes.js files in order to communicate when to generate api routes and html files
-require("./apiRoutes.js")(app);
-require("./htmlRoutes")(app);
+currentID = notes.length;
+
+// API Routes
+app.get("/api/notes", function (req, res) {
+    
+    return res.json(notes);
+});
+
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+
+    newNote["id"] = currentID +1;
+    currentID++;
+    console.log(newNote);
+
+    notes.push(newNote);
+
+    rewriteNotes();
+
+    return res.status(200).end();
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    res.send('DELETE request for /api/notes/:id')
+
+    var id = req.params.id;
+
+    var idLess = notes.filter(function (less) {
+        return less.id < id;
+    });
+
+    var idGreater = notes.filter(function (greater) {
+        return greater.id > id;
+    });
+
+    notes = idLess.concat(idGreater);
+
+    rewriteNotes();
+})
 
 
-// Starts the server to begin listening
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+// HTML Routes for index.html & notes.html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
+
+// Function to writeFile
+function rewriteNotes() {
+    fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
+        if (err) {
+            console.log("error")
+            return console.log(err);
+        }
+
+        console.log("Success!");
+    });
+}
+
+// Listen for PORT
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
